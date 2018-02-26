7c055a23f299364599ae86437ad94da5631252ef
vbazylevych <vbazylevych@playtika.com.ua>
2018-02-26 18:25:23 +0200

    hyutyu

---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class main {
+    static Integer a,b,c;
+    public static void main(String[] args) {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("http://it-ebooks.info/");
+
+    }
+}
